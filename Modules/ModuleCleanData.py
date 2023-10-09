547d2c9ef4e29d92861e7356c1330e118b2dce50
José Miguel Vilches <josemiguelvilchesfierro@gmail.com>
2023-10-09 18:06:31 +0100

    Merge remote-tracking branch 'origin/Import_data_csv' into Last_changes

--- Modules/ModuleCleanData.py
+++ Modules/ModuleCleanData.py
@@ -6,27 +6,51 @@ pd.set_option('display.max_columns', None)
 
 def CleanDataRent(dfRent):
     # EDA Exploratory data analysis
-    #print(dfRent.head())
-    #print(dfRent.shape)
-    #print(dfRent.dtypes)
+    # print(dfRent.head())
+    # print(dfRent.shape)
+    # print(dfRent.dtypes)
 
     # Change columns names
     dfRent = dfRent.rename(columns={'Number of Bedrooms': 'Number_of_bedrooms',
-                                'Property Type': 'Property_Type',
-                                'VALUE': 'Price'
-                                })
+                                    'Property Type': 'Property_Type',
+                                    'VALUE': 'Price'
+                                    })
 
-    ##Group by to delete columns
+    # Group by to delete columns
     # df_group = dfCensus.groupby(['Dublin_Postcodes','SubCat','S_Location','Location','RegionFilter'])['Price'].sum()
-    df_group = dfRent.groupby(['UNIT'])['Price'].sum()
 
-    ##Drop Columns
+    # Drop Columns
     dfRent = dfRent.drop(columns=['STATISTIC Label', 'UNIT'])
 
-    #Parse Type
-    #dfRent['Price'] = dfRent['Price'].fillna(0).astype('float')
+    # Create condition to clean Nulls rows in dfRent
 
-    return (dfRent)
+
+    indexDropRentBed = dfRent[
+        (dfRent['Number_of_bedrooms'] != 'All bedrooms')|
+        (dfRent['Property_Type'] != 'All property types')
+        ].index
+
+    indexDropRentPrice = dfRent[
+        (dfRent['Price'] == '') &
+        (dfRent['Year'].notnull()) &
+        (dfRent['Location'].notnull()) &
+        (dfRent['Number_of_bedrooms'] == 'All bedrooms') &
+        (dfRent['Property_Type'] == 'All property types')
+    ].index
+
+    dfRent = dfRent.drop(indexDropRentBed)
+    dfRent = dfRent.drop(indexDropRentPrice)
+
+    # indexDropRent = dfRent[(dfRent['Property_Type'] != 'All property types')].index
+    # dfRent = dfRent.drop(indexDropRent)
+
+    df_group = dfRent.groupby(['Number_of_bedrooms', 'Property_Type'])['Price'].sum()
+
+    # Parse Type
+    # dfRent['Price'] = dfRent['Price'].fillna(0).astype('float')
+
+
+    return dfRent
 
 
 def CleanDataCens(dfCensus):
@@ -45,9 +69,11 @@ def CleanDataCens(dfCensus):
                                         }
                                )
 
-    #Parse type to INT64
+    # Parse type to INT64
     dfCensus['CensusBothSex'] = dfCensus['CensusBothSex'].fillna(0).astype('int64')
     dfCensus['CensusMale'] = dfCensus['CensusMale'].fillna(0).astype('int64')
     dfCensus['CensusFemale'] = dfCensus['CensusFemale'].fillna(0).astype('int64')
+
+    censusGroup = dfCensus.groupby(['Year','County'])['CensusBothSex'].sum()
 
     return dfCensus
