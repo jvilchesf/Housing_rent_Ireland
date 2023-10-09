35196fa3b7b20cc3476a74e25d4a98019a04803f
José Miguel Vilches <josemiguelvilchesfierro@gmail.com>
2023-10-09 18:56:48 +0100

    Merge remote-tracking branch 'origin/Import_data_csv' into Last_changes

--- Modules/ModuleImportData.py
+++ Modules/ModuleImportData.py
@@ -5,11 +5,6 @@ import pandas as pd
 
 # Import Data
 
-# Define a function to clean characters
-def clean_string(s):
-    return str(s).replace('\r', '').replace('"', '')
-
-
 def ImportData_rent():
     url = "https://ws.cso.ie/public/api.jsonrpc?data=%7B%22jsonrpc%22:%222.0%22,%22method%22:%22PxStat.Data.Cube_API.ReadDataset%22,%22params%22:%7B%22class%22:%22query%22,%22id%22:%5B%5D,%22dimension%22:%7B%7D,%22extension%22:%7B%22pivot%22:null,%22codes%22:false,%22language%22:%7B%22code%22:%22en%22%7D,%22format%22:%7B%22type%22:%22JSON-stat%22,%22version%22:%222.0%22%7D,%22matrix%22:%22RIA02%22%7D,%22version%22:%222.0%22%7D%7D"
     # Define the JSON-RPC request payload
